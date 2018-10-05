7090889ec93e481b26a05d66be157401dc3d9c46
Shane Vander Meer <shane.vander.meer@globalrelay.net>
2018-10-05 12:46:47 -0700

    Logic logic logic

---
+++ Ticker.Logic/Observers/StockObserver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Ticker.Entities;
+
+namespace Ticker.Logic.Observers
+{
+    public class StockObserver : IObserver<IStock>
+    {
+        private readonly IObservable<IStock> _observable;
+        public IStock Stock { get; private set; }
+
+        public StockObserver(IStock stock)
+        {
+            Stock = stock;
+            
+        }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(IStock value)
+        {
+            Stock = value;
+        }
+    }
+}
---
+++ Ticker.Logic/Utils/Randomizer.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ticker.Logic.Utils
+{
+    public class Randomizer
+    {
+        private static readonly Random Random = new Random();
+        private readonly Random _random;
+
+        public Randomizer()
+        {
+            _random = new Random(Random.Next());
+        }
+
+        public T GetRandomItemFrom<T>(IList<T> items)
+        {
+            if (!items.Any()) return default(T);
+            var count = items.Count - 1;
+            
+            return count == 0 ? items[0] : items[_random.Next(count)];
+        }
+    }
+}
