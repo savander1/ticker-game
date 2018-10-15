b6ad2168c809a311ba7eda5031c3dd8576c94a20
Shane Vander Meer <shane.vander.meer@globalrelay.net>
2018-10-15 16:28:35 -0700

    adding game classes

---
+++ Ticker.Logic/Game.cs
@@ -0,0 +1,20 @@
+namespace Ticker.Logic
+{
+    public class Game
+    {
+        public Game(GameConfig config)
+        {
+
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Delete()
+        {
+            
+        }
+    }
+}
